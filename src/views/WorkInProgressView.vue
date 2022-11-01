509904a05c0abe19d2f366ead0b3caf403b27d45
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-11-01 14:16:59 +0100

    Fixed Responsive Issues. Adjusted the mobile version. Wrote Comments and removed unused console logs & Debuggers.

--- src/views/WorkInProgressView.vue
+++ src/views/WorkInProgressView.vue
@@ -4,6 +4,7 @@
         <div
             class="footer-container flex justify-center items-center text-text-color flex-wrap w-4/5 md:w-3/5 text-2xl gap-5 pb-10">
             <label class="form-label self-start" for="customFile">Upload your own image</label>
+            <!-- Uploading the file using the uploadFile function in useMapStorage.js -->
             <input @change="uploadFile" type="file" class="self-start w-full" id="customFile" />
         </div>
     </section>
@@ -14,6 +15,4 @@ import useMapStorage from "@/modules/useMapStorage.js";
 import MapComponent from "@/components/wip/MapComponent.vue";
 
 const { uploadFile } = useMapStorage();
-
-
 </script>
