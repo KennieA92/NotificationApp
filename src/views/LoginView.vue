0995a765c33d99666650a8bb7da14c64e1a5f874
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-11-01 14:19:05 +0100

    Responsive

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="login h-100 flex justify-center items-center flex-col">
+  <div class="h-screen w-full flex justify-center items-center flex-col">
     <h1 class="text-white text-2xl mb-3">Login</h1>
-    <form @submit.prevent="login" class="flex w-10/12 :md-w-6/12 flex-col justify-center items-center gap-2">
-      <input class="w-10/12 py-2 text-center" type="text" placeholder="Email" v-model="email" />
-      <input class="w-10/12 py-2 text-center" type="password" placeholder="Password" v-model="password" />
+    <form
+      @submit.prevent="login"
+      class="flex w-10/12 :md-w-6/12 flex-col justify-center items-center gap-2"
+    >
+      <input
+        class="w-10/12 py-2 text-center"
+        type="text"
+        placeholder="Email"
+        v-model="email"
+      />
+      <input
+        class="w-10/12 py-2 text-center"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+      />
       <div class="">
-        <input type="submit" value="Login"
-          class="bg-accent font-headers text-xl text-white px-4 py-2 mx-10 my-4 rounded-md" />
-        <RouterLink to="/register" class="bg-accent font-headers text-xl text-white px-4 py-2 mx-10 my-4 rounded-md">
-          Register</RouterLink>
+        <input
+          type="submit"
+          value="Login"
+          class="bg-accent font-headers text-xl text-white px-4 py-3 mx-2 md:mx-10 my-4 rounded-md"
+        />
+        <RouterLink
+          to="/register"
+          class="bg-accent font-headers text-xl text-white px-4 py-3 mx-2 md:mx-10 my-4 rounded-md"
+        >
+          Register</RouterLink
+        >
       </div>
     </form>
   </div>
